03f8ff42229119e00b96ddb7210bd4ef63c9d35e
Chang-Hung Liang <eliang.cs@gmail.com>
2014-03-25 23:23:03 +0800

    'Exclude' feature wip

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,10 +3,17 @@ import bisheng
 import unittest
 
 
-class APITest(unittest.TestCase):
+class AddSpacesTest(unittest.TestCase):
 
     def test_simple_cases(self):
         self.assertEqual(bisheng.add_spaces(u'測試test'), u'測試 test')
+        self.assertEqual(bisheng.add_spaces(u'Hello世界'), u'Hello 世界')
+        self.assertEqual(bisheng.add_spaces(u'阿Q正傳'), u'阿 Q 正傳')
+
+    def test_exclude(self):
+        self.assertEqual(bisheng.add_spaces(u'方[括]孤'), u'方 [括] 孤')
+        self.assertEqual(bisheng.add_spaces(u'方[括]孤', exclude='['), u'方[括] 孤')
+        self.assertEqual(bisheng.add_spaces(u'方[括]孤', exclude='[]'), u'方[括]孤')
 
 
 if __name__ == '__main__':
