69971074cfb2d92c8a9740ceb511a676b3c8dfb3
Chang-Hung Liang <eliang.cs@gmail.com>
2014-03-29 22:39:28 +0800

    Prepare for publish

--- setup.py
+++ setup.py
@@ -1,6 +1,36 @@
-import bisheng
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-from distutils.core import setup
+import codecs
+import os
+import re
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+# Read the version number from a source file.
+# Why read it, and not import?
+# see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
+def find_version(*file_paths):
+    # Open in Latin-1 so that we avoid encoding errors.
+    # Use codecs.open for Python 2 compatibility
+    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
+        version_file = f.read()
+
+    # The version line must have the form
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string')
+
+
+def read_description(filename):
+    with codecs.open(filename, encoding='utf-8') as f:
+        return f.read()
 
 
 def parse_requirements(filename):
@@ -11,14 +41,16 @@ def parse_requirements(filename):
 
 setup(
     name='bisheng',
-    version=bisheng.__version__,
+    version=find_version('bisheng', '__init__.py'),
     url='https://github.com/eliangcs/bisheng',
     description='A set of utility functions for processing Chinese text.',
-    long_description=open('README.rst').read(),
+    long_description=read_description('README.rst'),
     author='Chang-Hung Liang',
     author_email='eliang.cs@gmail.com',
     license='BSD',
     packages=['bisheng'],
+    package_data={'bisheng': ['*.txt']},
+    include_package_data=True,
     install_requires=parse_requirements('requirements.txt'),
     classifiers=[
         'Development Status :: 4 - Beta',
