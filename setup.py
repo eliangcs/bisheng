b2be06875c392232026584611feff96865dc747a
Chang-Hung Liang <eliang.cs@gmail.com>
2014-03-29 20:47:03 +0800

    Add classifiers in setup.py

--- setup.py
+++ setup.py
@@ -13,4 +13,25 @@ setup(
     author_email='eliang.cs@gmail.com',
     license='BSD',
     packages=['bisheng'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
