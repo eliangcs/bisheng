2407604c18681c068f19fc994b4f0ff433aa125a
Chang-Hung Liang <eliang.cs@gmail.com>
2014-03-29 20:39:45 +0800

    Updte description

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='bisheng',
     version=bisheng.__version__,
     url='https://github.com/eliangcs/bisheng',
-    description='Adds spaces between Chinese/Japanese/Korean and halfwidth characters.',
+    description='A set of utility functions for processing Chinese text.',
     long_description=open('README.rst').read(),
     author='Chang-Hung Liang',
     author_email='eliang.cs@gmail.com',
